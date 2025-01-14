e5ccef35b5bccbf12b1ae9ec52141ed4b477949d
AyumuKawahara <ayumu_note@outlook.jp>
2025-01-14 18:49:12 +0900

    お問い合わせ完了英語対応

--- src/app/[language]/contact/completed/page.tsx
+++ src/app/[language]/contact/completed/page.tsx
@@ -1,27 +1,44 @@
 import Link from 'next/link'
+import { twMerge } from 'tailwind-merge'
 
 type Props = {
-  language: 'ja' | 'en'
+  params: Promise<{
+    language: 'ja' | 'en'
+  }>
 }
 
-export default function ContactCompletedPage({ language }: Props) {
+export default async function ContactCompletedPage({ params }: Props) {
+  const { language } = await params
   return (
     <div className="py-[80px] md:py-[120px] px-4">
-      <div className="bg-white max-w-[800px] w-full mx-auto px-4 md:px-10 py-8 md:py-10 rounded-md flex flex-col gap-y-10 text-center">
+      <div className="bg-white max-w-[800px] w-full mx-auto px-4 md:px-10 py-8 md:py-10 rounded-md flex flex-col gap-y-6 text-center">
         <h1 className="text-center text-2xl md:text-3xl font-bold">
-          お問い合わせ完了
+          {language === 'ja' ? 'お問い合わせ完了' : 'Inquiry Completed'}
         </h1>
         <div className="leading-7 md:leading-8 tracking-wide text-sm md:text-base">
-          <p>お問い合わせ頂きありがとうございます。</p>
-          <p>お問い合わせ内容を確認し、</p>
-          <p>数日以内に担当者よりメールまたは電話にてご連絡させて頂きます。</p>
-          <p>今しばらくお待ちください。</p>
+          {language === 'ja' ? (
+            <>
+              <p>お問い合わせ頂きありがとうございます。</p>
+              <p>お問い合わせ内容を確認し、</p>
+              <p>
+                数日以内に担当者よりメールまたは電話にてご連絡させて頂きます。
+              </p>
+              <p>今しばらくお待ちください。</p>
+            </>
+          ) : (
+            <>
+              <p>Thank you for your inquiry!</p>
+              <p>We will review the details of your inquiry</p>
+              <p>and get back to you via email or phone within a few days.</p>
+              <p>We kindly ask for your patience in the meantime.</p>
+            </>
+          )}
         </div>
         <Link
-          href="/ja"
+          href={language === 'ja' ? '/ja' : '/en'}
           className="flex items-center gap-x-1 font-bold text-moss hover:underline underline-offset-2 mx-auto"
         >
-          トップページへ戻る
+          {language === 'ja' ? 'トップページへ戻る' : 'Back to Homepage'}
         </Link>
       </div>
     </div>
