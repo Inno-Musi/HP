2ce9b02a3b61b50d10516918b90455d89fcda4c3
AyumuKawahara <ayumu_note@outlook.jp>
2025-04-22 00:24:04 +0900

    お問い合わせフォーム送信時にSlack通知機能を追加し、送信失敗時にも通知を行うように修正

--- src/features/contact/_actions/submit-contact-form-ja.ts
+++ src/features/contact/_actions/submit-contact-form-ja.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { notifySlack } from '@/services/slack/notify-slack'
 import { redirect } from 'next/navigation'
 import { schemaContactFormJa } from '../_helpers/schema-contact-form-ja'
 
@@ -43,31 +44,46 @@ export const submitContactFormJa = async (
     inquiryDetails,
   } = result.data
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_ENV === 'development' ? 'http://' : 'https://'}${process.env.NEXT_PUBLIC_VERCEL_URL}/api/email`,
-    {
-      method: 'POST',
-      headers: {
-        'X-API-KEY': process.env.X_API_KEY ?? '',
-      },
-      body: JSON.stringify({
-        template: 'contact',
-        props: {
-          name: `${lastName} ${firstName} (${lastNameKana} ${firstNameKana})`,
-          affiliation: affiliation,
-          email: email,
-          phoneNumber: phoneNumber,
-          inquiryType: inquiryType,
-          inquiryDetails: inquiryDetails,
+  const [_resSlack, res] = await Promise.all([
+    notifySlack(
+      `お問い合わせがありました
+      【名前】: ${lastName} ${firstName} (${lastNameKana} ${firstNameKana})
+      【所属】: ${affiliation}
+      【メールアドレス】: ${email}
+      【電話番号】: ${phoneNumber}
+      【問い合わせ種類】: ${inquiryType}
+      【問い合わせ内容】: ${inquiryDetails}
+      `,
+    ),
+    fetch(
+      `${process.env.NEXT_PUBLIC_VERCEL_ENV === 'development' ? 'http://' : 'https://'}${process.env.NEXT_PUBLIC_VERCEL_URL}/api/email`,
+      {
+        method: 'POST',
+        headers: {
+          'X-API-KEY': process.env.X_API_KEY ?? '',
         },
-        subject: '【musicoホームページ】お問い合わせがありました',
-      }),
-    },
-  )
+        body: JSON.stringify({
+          template: 'contact',
+          props: {
+            name: `${lastName} ${firstName} (${lastNameKana} ${firstNameKana})`,
+            affiliation: affiliation,
+            email: email,
+            phoneNumber: phoneNumber,
+            inquiryType: inquiryType,
+            inquiryDetails: inquiryDetails,
+          },
+          subject: '【musicoホームページ】お問い合わせがありました',
+        }),
+      },
+    ),
+  ])
 
   const now = new Date().getTime()
 
   if (!res.ok) {
+    await notifySlack(
+      'お問い合わせの送信に失敗しました。速やかに確認してください。',
+    )
     return {
       toast: {
         status: 'error',
